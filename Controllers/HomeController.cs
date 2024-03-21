565113053a287c6262b3d98cff8da27bd168b652
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-03-21 15:10:11 -0400

    Added Controller Actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,12 +12,47 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-    
+
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
-    
+
+    [HttpPost]
+    public IActionResult Index(double firstNum, double secondNum, string mathOp)
+    {
+        if (mathOp == "add")
+        {
+            ViewBag.Result = $"{firstNum} + {secondNum} = {firstNum + secondNum}";
+        }
+        else if (mathOp == "subtract")
+        {
+            ViewBag.Result = $"{firstNum} - {secondNum} = {firstNum - secondNum}";
+        }
+        else if (mathOp == "multiply")
+        {
+            ViewBag.Result = $"{firstNum} * {secondNum} = {firstNum * secondNum}";
+        }
+        else if (mathOp == "divide")
+        {
+            if (secondNum == 0)
+            {
+                ViewBag.Result = "Dividing to 0 will result to infinite value";
+            }
+            else
+            {
+                ViewBag.Result = $"{firstNum} / {secondNum} = {firstNum / secondNum}";
+            }
+        }
+        else
+        {
+            ViewBag.Result = $"%A{mathOp} not supported";
+        }
+
+        return View();
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
