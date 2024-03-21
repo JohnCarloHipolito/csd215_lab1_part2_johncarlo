3e144f684203539a51af24978325b812c430805f
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-03-21 14:34:44 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace csd215_lab1_part2_johncarlo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
